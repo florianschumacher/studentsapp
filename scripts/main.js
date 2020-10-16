6676d959066dd523dcc170e7b296ab87545d5569
Oliver Starckjohann <oliverstarckjohann@gmail.com>
2020-10-16 07:55:48 +0200

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+import navigateTo from './navigation'
+
+document
+    .querySelectorAll('.nav__button')
+    .forEach(element => {
+        console.log(element)
+        element.addEventListener('click', (event) => {
+            event.preventDefault()
+            console.log(element, event)
+            navigateTo(event.target.getAttribute('data-href'))
+        })
+    })
